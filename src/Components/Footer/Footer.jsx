e9211206c7f80d4d033b2206f6194add22118341
Pial17 <pialrafsan17@gmail.com>
2024-12-03 19:48:45 +0600

    Footer added

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import './Footer.css';
+import logo1 from '../../assets/logo1.png';
+import user_icon from '../../assets/user_icon.svg';
+
+const Footer = () => {
+    return (
+        <div className='footer'>
+            <div className="footer-top">
+                <div className="footer-top-left">
+                    <img src={logo1} alt=''/>
+                    <p>I am a beginner frontend developer learning by myself and creating new projects everyday.</p>
+                </div>
+                <div className="footer-top-right">
+                    <div className="footer-email-input">
+                        <img src={user_icon}alt=''/>
+                        <input type='email' placeholder='Enter your email'/>
+                    </div>
+                    <div className="footer-subscribe">
+                        Subscribe
+                    </div>
+                </div>
+            </div>
+            <hr/>
+            <div className="footer-bottom">
+                <p className="footer-bottom-left">
+                © 2024 Pial Rafsan. All rights reserved.
+                </p>
+            </div>
+            <div className="footer-bottom-right">
+                <p>Term of services</p>
+                <p>Privecy Policy</p>
+                <p>Connect with me</p>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
