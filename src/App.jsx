7a11b1fd031b00e834381e0476ddb27436c74140
Pial17 <pialrafsan17@gmail.com>
2024-12-03 19:47:46 +0600

    Other files are added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import About from "./Components/About/About"
+import Contact from "./Components/Contact/Contact"
+import Footer from "./Components/Footer/Footer"
 import Hero from "./Components/Hero/Hero"
 import Mywork from "./Components/Mywork/Mywork"
 import Navbar from "./Components/Navbar/Navbar"
@@ -13,6 +15,8 @@ function App() {
       <About/>
       <Services/>
       <Mywork/>
+      <Contact/>
+      <Footer/>
     </div>
   )
 }
